86029f7769361e3bc9c2d929dec7cfd016bb0f8e
ICELOLLYjp <c22w6f9b7m@privaterelay.appleid.com>
2026-09-10 09:40:53 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,14 +1,11 @@
-// Firebase Console > Project settings > Your apps > Web app
-// の値をここへ貼り付けます。
-// GitHubへ公開する場合でも Firebase Security Rules と Authentication を必ず使用してください。
-
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyA1AZlnCYr5cfHG3HDcyvG17jokBnr5lO8",
+  authDomain: "t-shirtstock.firebaseapp.com",
+  projectId: "t-shirtstock",
+  storageBucket: "t-shirtstock.firebasestorage.app",
+  messagingSenderId: "485805702075",
+  appId: "1:485805702075:web:f9d8668ca57d9f58c4229e",
+  measurementId: "G-TXEHB9FGSR"
 };
 
 export const firebaseEnabled = Boolean(
